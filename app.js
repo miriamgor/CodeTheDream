34544949990d12555054199bb96b1759a933df13
Miriam Gordillo-Chavez <miriamgordillochavez@gmail.com>
2024-03-26 00:44:46 -0500

    sample api call

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-// // Define the API URL
-// const apiUrl = 'https://api.artic.edu/api/v1/artworks';
+// Define the API URL
+const apiUrl = 'https://api.artic.edu/api/v1/artworks/search?q=cats';
 
-// // Make a GET request
-// fetch(apiUrl)
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
+// Make a GET request
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
 
 
 
@@ -39,27 +39,27 @@
 
 
 
-  const apiKey = 'your_openweathermap_api_key';
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
+// const apiKey = 'your_openweathermap_api_key';
+// const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
 
-const outputElement = document.getElementById('weather-output');
+// const outputElement = document.getElementById('weather-output');
 
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    const temperature = data.main.temp;
-    const description = data.weather[0].description;
-    const location = data.name;
-    outputElement.innerHTML = `<p>Temperature in ${location}: ${temperature}°C</p>
-                               <p>Weather: ${description}</p>`;
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+// fetch(apiUrl)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     const temperature = data.main.temp;
+//     const description = data.weather[0].description;
+//     const location = data.name;
+//     outputElement.innerHTML = `<p>Temperature in ${location}: ${temperature}°C</p>
+//                                <p>Weather: ${description}</p>`;
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
 
   
